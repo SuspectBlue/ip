b7c7c0859a6c918524408b95292af62ac8b719ab
SuspectBlue <jovanyosh17@gmail.com>
2025-01-23 14:47:20 +0800

    Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Pelops II User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/PelopsII.java
@@ -0,0 +1,8 @@
+public class PelopsII {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Pelops II");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+        // System.out.println("Running Java version: " + System.getProperty("java.version"));
+    }
+}
