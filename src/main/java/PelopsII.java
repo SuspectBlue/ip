3b2179e52069a3e1a15726f5d6c1557ff7153bb3
SuspectBlue <jovanyosh17@gmail.com>
2025-01-23 15:24:32 +0800

    Add, List

--- src/main/java/PelopsII.java
+++ src/main/java/PelopsII.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class PelopsII {
     public static void main(String[] args) throws IOException {
@@ -9,15 +10,22 @@ public class PelopsII {
         System.out.println("Hello! I'm Pelops II");
         System.out.println("What can I do for you?");
 
+        ArrayList<String> list = new ArrayList<>();
+
         while (true) {
             String input = br.readLine();
 
-            if(input.equals("bye")) {
-                System.out.println("Pelops II: Bye. Hope to see you again soon!");
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for(int i = 1; i <= list.size(); i++) {
+                    System.out.println(i + ". " + list.get(i - 1));
+                }
+            } else {
+                list.add(input);
+                System.out.println("added: " + input);
             }
-
-            System.out.println("Pelops II: " + input);
         }
         // System.out.println("Running Java version: " + System.getProperty("java.version"));
         br.close();
