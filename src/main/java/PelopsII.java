a37722e204c7f7d7f5794e085e04b5c45849a9a1
SuspectBlue <jovanyosh17@gmail.com>
2025-01-23 18:15:10 +0800

    Implemented Delete for TaskList

--- src/main/java/PelopsII.java
+++ src/main/java/PelopsII.java
@@ -43,41 +43,57 @@ public class PelopsII {
                 } else if (action[0].equals("todo")) {
                     if(action.length == 1) {
                         throw new PelopsIIException("ToDo tasks require a description. For example: todo <description>");
-                    } else {
-                        String description = input.split("todo ")[1];
-                        list.add(new ToDo(description));
-                        int listSize = list.size();
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(list.get(listSize-1));
-                        System.out.println("Now you have " + listSize + (listSize == 1 ? " task in the list." : " tasks in the list."));
                     }
+                    String description = input.split("todo ")[1];
+                    list.add(new ToDo(description));
+                    int listSize = list.size();
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(list.get(listSize-1));
+                    System.out.println("Now you have " + listSize + (listSize == 1 ? " task in the list." : " tasks in the list."));
                 } else if (action[0].equals("deadline")) {
                     if(action.length == 1) {
                         throw new PelopsIIException("Deadline tasks must include both a description and a specified deadline time. For example: deadline <description> /by <deadline>");
-                    } else {
-                        String data = input.split("deadline ")[1];
-                        String description = data.split(" /by ")[0];
-                        String byDate = data.split(" /by ")[1];
-                        list.add(new Deadline(description, byDate));
-                        int listSize = list.size();
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(list.get(listSize-1));
-                        System.out.println("Now you have " + listSize + (listSize == 1 ? " task in the list." : " tasks in the list."));
                     }
+                    String data = input.split("deadline ")[1];
+                    String description = data.split(" /by ")[0];
+                    String byDate = data.split(" /by ")[1];
+                    list.add(new Deadline(description, byDate));
+                    int listSize = list.size();
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(list.get(listSize-1));
+                    System.out.println("Now you have " + listSize + (listSize == 1 ? " task in the list." : " tasks in the list."));
                 } else if(action[0].equals("event")) {
                     if(action.length == 1) {
                         throw new PelopsIIException("Event tasks must include a description, event start time and event end time. For example: event <description> /from <start_time> /to <end_time>");
-                    } else {
-                        String data = input.split("event ")[1];
-                        String description = data.split(" /from ")[0];
-                        String fromTime = data.split(" /from ")[1].split(" /to ")[0];
-                        String toTime = data.split(" /to ")[1];
-                        list.add(new Event(description, fromTime, toTime));
-                        int listSize = list.size();
-                        System.out.println("Got it. I've added this task:");
-                        System.out.println(list.get(listSize-1));
-                        System.out.println("Now you have " + listSize + (listSize == 1 ? " task in the list." : " tasks in the list."));
                     }
+                    String data = input.split("event ")[1];
+                    String description = data.split(" /from ")[0];
+                    String fromTime = data.split(" /from ")[1].split(" /to ")[0];
+                    String toTime = data.split(" /to ")[1];
+                    list.add(new Event(description, fromTime, toTime));
+                    int listSize = list.size();
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(list.get(listSize-1));
+                    System.out.println("Now you have " + listSize + (listSize == 1 ? " task in the list." : " tasks in the list."));
+                } else if (action[0].equals("delete")) {
+                    if (action.length == 1) {
+                        throw new PelopsIIException("You must specify an index when deleting a task");
+                    }
+                    if (action.length > 2) {
+                        throw new PelopsIIException("You have specified too many parameters for the delete command");
+                    } 
+                    if (!isNumeric(action[1])) {
+                        throw new PelopsIIException("The index must be a numeric number");
+                    }
+
+                    int index = Integer.parseInt(action[1]) - 1;
+                    if (index < 0 || index >= list.size()) {
+                        throw new PelopsIIException("The index is out of range of the task list");
+                    }
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println(list.remove(index));
+                    System.out.println("Now you have " + list.size() + (list.size() == 1 ? " task in the list." : " tasks in the list."));
+                    
                 } else {
                     throw new PelopsIIException("Unrecognised command. Please input a valid command.");
                 }
