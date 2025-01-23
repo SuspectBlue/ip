5e490ee16f567399a5dfe2012fa7ef0b4dd7d71a
SuspectBlue <jovanyosh17@gmail.com>
2025-01-23 15:03:06 +0800

    Echo

--- src/main/java/PelopsII.java
+++ src/main/java/PelopsII.java
@@ -1,8 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class PelopsII {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("Hello! I'm Pelops II");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = br.readLine();
+            System.out.println("Pelops II: " + input);
+
+            if(input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
         // System.out.println("Running Java version: " + System.getProperty("java.version"));
+        br.close();
     }
 }
