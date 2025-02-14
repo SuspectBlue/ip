3493a2f3e7052a6ca5c757defd32b2ec5627717d
SuspectBlue <jovanyosh17@gmail.com>
2025-02-14 14:22:50 +0800

    Update test cases

--- src/test/java/pelopsii/task/TaskTest.java
+++ src/test/java/pelopsii/task/TaskTest.java
@@ -13,13 +13,13 @@ public class TaskTest {
 
     @Test
     public void deadline_task_success() {
-        Assertions.assertEquals("[D][ ] deadline 1 (by: 30 May 2025 11:00pm)", 
+        Assertions.assertEquals("[D][ ] deadline 1 (by: 30 May 2025 11:00PM)", 
         new Deadline("deadline 1", LocalDateTime.parse("2025-05-30 2300", DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"))).toString());
     }
 
     @Test
     public void event_task_success() {
-        Assertions.assertEquals("[E][ ] event (from: 30 May 2025 11:00pm to: 31 May 2025 11:00pm)",
+        Assertions.assertEquals("[E][ ] event (from: 30 May 2025 11:00PM to: 31 May 2025 11:00PM)",
                 new Event("event", LocalDateTime.parse("2025-05-30 2300", DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm")), LocalDateTime.parse("2025-05-31 2300", DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"))).toString());
     }
 }
